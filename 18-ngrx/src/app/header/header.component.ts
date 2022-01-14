18f202c4c1e5dea93b0d4ee2bdd4a817021966b6
Cong Anh Nguyen <nguyenconganh170696@gmail.com>
2022-01-14 09:46:19 +0700

    .

--- 18-ngrx/src/app/header/header.component.ts
+++ 18-ngrx/src/app/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { map, Subscription } from 'rxjs';
 
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
@@ -14,11 +17,17 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   constructor(
     private dataStorageService: DataStorageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
   ) {}
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe(user => {
+    this.userSub = this.store.select('auth')
+    .pipe(
+      map(authState => {
+        return authState.user;
+      })
+    ).subscribe((user) => {
       this.isAuthenticated = !!user;
       console.log(!user);
       console.log(!!user);
